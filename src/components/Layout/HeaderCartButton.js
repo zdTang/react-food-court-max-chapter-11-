cdd73389dc0677f0e9647e5e8ccd871ea02ad303
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2021-11-04 07:42:59 -0400

    11-004 Adding the _Cart_ Button Component

---
+++ src/components/Layout/HeaderCartButton.js
@@ -0,0 +1,15 @@
+import React from "react";
+import CartIcon from "../Cart/CartIcon";
+import classes from "./HeaderCartButton.module.css";
+
+export const HeaderCartButton = () => {
+  return (
+    <button className={classes.button}>
+      <span className={classes.icon}>
+        <CartIcon />
+      </span>
+      <span>Your Cart</span>
+      <span className={classes.badge}>88</span>
+    </button>
+  );
+};
