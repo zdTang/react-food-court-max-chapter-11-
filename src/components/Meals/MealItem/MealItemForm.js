3302c9acb440a4dd09a12593881bb3656db4b26b
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2021-11-04 12:26:53 -0400

    11-007 Adding a Form
    create a Input which is very nice and powerful
    
    it can capsulate Label and Input and styles ,   reusable !

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,10 +1,20 @@
 import React from "react";
 import classes from "./MealItemForm.module.css";
-
+import Input from "../../UI/Input";
 const MealItemForm = (props) => {
   return (
     <form className={classes.form}>
-      <input type="text"></input>
+      <Input
+        label="Amount"
+        input={{
+          id: "amount",
+          type: "number",
+          min: "1",
+          max: "5",
+          step: "1",
+          defaultValue: "1",
+        }}
+      />
       <button>+ Add</button>
     </form>
   );
