2659efc4c2ab772f963969acea0f9a594570a046
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2021-11-04 10:50:51 -0400

    Add Card

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,5 +1,6 @@
 import React from "react";
 import classes from "./AvailableMeals.module.css";
+import Card from "../UI/Card";
 const DUMMY_MEALS = [
   {
     id: "m1",
@@ -32,7 +33,9 @@ const AvailableMeals = () => {
   ));
   return (
     <section className={classes.meals}>
-      <ul>{mealsList}</ul>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
     </section>
   );
 };
