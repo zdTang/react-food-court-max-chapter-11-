df5db134a3d9e489bfd790315c14667d74225466
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2021-11-04 07:20:25 -0400

    11-003 Adding a _Header_ Component

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,17 @@
+import { Fragment } from "react";
+import mealsImage from "../../assets/meals.jpg";
+import classes from "./Header.module.css";
+
+export default function Header() {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <button>Cart</button>
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealsImage} alt="food court" />
+      </div>
+    </Fragment>
+  );
+}
