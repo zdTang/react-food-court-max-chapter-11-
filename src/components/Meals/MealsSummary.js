47d723f0d9b7b940c4a00264c674664cb169f546
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2021-11-04 09:44:30 -0400

    11-005 Adding a _Meals_ Component

---
+++ src/components/Meals/MealsSummary.js
@@ -0,0 +1,25 @@
+import React from "react";
+import classes from "./MealsSummary.module.css";
+
+const MealsSummary = () => {
+  return (
+    <section className={classes.summary}>
+      <h2>Delicious Food, Delivered To You</h2>
+      <p>
+        Consectetur consectetur nulla veniam proident consectetur est sint velit
+        in exercitation exercitation consequat minim veniam. Ea quis mollit
+        excepteur id esse eu. Tempor elit eu esse commodo anim. Sint ullamco
+        exercitation commodo eu.
+      </p>
+      <p>
+        Qui sunt ex id non aliquip esse commodo aliqua anim ea. Dolor aute
+        voluptate cillum qui duis in nostrud dolor aute non. Anim incididunt
+        labore fugiat ullamco. Non tempor nisi est ad amet enim. Incididunt
+        consequat Lorem officia non proident aliquip ullamco velit. Sint sint in
+        cillum aliqua laborum commodo reprehenderit cillum.
+      </p>
+    </section>
+  );
+};
+
+export default MealsSummary;
