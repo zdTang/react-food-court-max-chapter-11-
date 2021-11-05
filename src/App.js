77d64a6c225ff63800211a825dd04351c0208e99
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2021-11-04 20:58:24 -0400

    Add a Cart component
    1.   setup mount point in the index.html
    2.  create a black cover component to cover the web pate. Z -index
    3,  create a information component on the cover  Z-index should higher
    3,  use ReactDOM.createPortal to mount the component to the HTML

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Fragment } from "react";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/Cart";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <main>
         <Meals />
       </main>
+      <Cart />
     </Fragment>
   );
 }
--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,25 +1,46 @@
 import React from "react";
 import classes from "./Cart.module.css";
+import ReactDOM from "react-dom";
 
-function Cart() {
+function Cart(props) {
   const cartItems = (
-    <ul className={classes.cart - items}>
+    <ul className={classes["cart-items"]}>
       {[{ id: "c1", name: "Sushi", price: 12.99 }].map((item) => (
-        <li>{item.name}</li>
+        <li key={item.id}>{item.name}</li>
       ))}
     </ul>
   );
+
+  const Backdrop = (props) => {
+    return <div className={classes.backdrop} onClick={props.onConfirm} />;
+  };
+
+  const CartContent = () => {
+    return (
+      <div className={classes.modal}>
+        {cartItems}
+        <div className={classes.total}>
+          <span>Total Amount</span>
+          <span>35.62</span>
+        </div>
+        <div className={classes.actions}>
+          <button className={classes["button--alt"]}>Close</button>
+          <button className={classes.button}>Order</button>
+        </div>
+      </div>
+    );
+  };
+
   return (
     <div>
-      {cartItems}
-      <div className={classes.total}>
-        <span>Total Amount</span>
-        <span>35.62</span>
-      </div>
-      <div className={classes.actions}>
-        <button className={classes["button--alt"]}>Close</button>
-        <button className={classes.button}>Order</button>
-      </div>
+      {ReactDOM.createPortal(
+        <Backdrop onConfirm={props.onConfirm} />,
+        document.getElementById("backdrop-root")
+      )}
+      {ReactDOM.createPortal(
+        <CartContent />,
+        document.getElementById("overlay-root")
+      )}
     </div>
   );
 }
