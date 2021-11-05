c5e21753dd70c78e25195c9f4a142481a64bb338
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2021-11-05 15:45:15 -0400

    Use context to pass data other then props chain
    1.  use useContext to pass EventHandler to son component
    2.  In the context provider, must use "value={ }" format to pass value

--- src/App.js
+++ src/App.js
@@ -2,16 +2,25 @@ import React, { Fragment } from "react";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
-
+import { useState } from "react";
+import CartContext from "./components/store/cart-context";
 function App() {
+  const [isShowCart, setIsShowCart] = useState(false);
+  const showCart = () => {
+    setIsShowCart(true);
+  };
+
+  // "value" is key word, must use it.
   return (
-    <Fragment>
-      <Header />
-      <main>
-        <Cart />
-        <Meals />
-      </main>
-    </Fragment>
+    <CartContext.Provider value={{ toggle: showCart }}>
+      <Fragment>
+        <Header />
+        <main>
+          {isShowCart && <Cart />}
+          <Meals />
+        </main>
+      </Fragment>
+    </CartContext.Provider>
   );
 }
 
--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,10 +1,17 @@
 import React from "react";
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
-
+import { useContext } from "react";
+import CartContext from "../store/cart-context";
 export const HeaderCartButton = () => {
+  const ctx = useContext(CartContext);
+
+  const clickHandler = () => {
+    ctx.toggle();
+  };
+
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={clickHandler}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
