a3f1372b1d9ecebbc61ee65c3aa7ad99acfa02db
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2021-11-05 18:58:39 -0400

    011-013 Using the Context-work on HeaderCartButton

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,19 +1,20 @@
-import React from 'react'
-import CartContext from './cart-context'
+import React from "react";
+import CartContext from "./cart-context";
 
 const CartProvider = (props) => {
+  const addItemToCartHandler = (item) => {};
+  const removeItemToCartHandler = (item) => {};
+  const cartContext = {
+    items: [],
+    totalAmount: 0,
+    addItem: addItemToCartHandler,
+    removeItem: removeItemToCartHandler,
+  };
+  return (
+    <CartContext.Provider value={cartContext}>
+      {props.children}
+    </CartContext.Provider>
+  );
+};
 
-    const addItemToCartHandler=item=>{}
-    const removeItemToCartHandler=item=>{}
-    const cartContext={
-        item:[],
-        totalAmount:0,
-        addItem:addItemToCartHandler,
-        removeItem:removeItemToCartHandler,
-    }
-    return <CartContext.Provider value={...cartContext}>
-        {props.children}
-    </CartContext.Provider>;
-}
-
-export default CartProvider
+export default CartProvider;
