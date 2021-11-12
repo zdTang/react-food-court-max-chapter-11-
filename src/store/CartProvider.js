c8565a2b57cee57c287c6b46a78457c9e9ea1da0
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2021-11-12 17:22:34 -0500

    11-014 Adding a Cart Reducer
    As the state is a little complicated.
    We use reducer to process state here.
    
    Try to understand how userReducer works.
    
    1, create a default state
    2, create a Reducer =>  update state based on different "Condition"
    3, use [state, dispatch]=useReducer(reducer, defaultState)

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,15 +1,63 @@
 import React from "react";
 import CartContext from "./cart-context";
+import { useReducer } from "react";
+
+// create default cart state
+const defaultCartState = {
+  items: [],
+  totalAmount: 0,
+};
+
+// create Reducer function first
+// the first argument is passed by the React, which is old state snapshot
+const cartReducer = (state, action) => {
+  if (action.type === "ADD") {
+    // The concat() method is used to merge two or more arrays.
+    // This method does not change the existing arrays,
+    // but instead returns a new array
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/concat
+    const updatedItems = state.items.concat(action.item);
+    const updatedTotalAmount =
+      state.totalAmount + action.item.price * action.item.amount;
+    return {
+      items: updatedItems,
+      amount: updatedTotalAmount,
+    };
+  }
+  return defaultCartState;
+};
+// use Reducer:   [state,dispatch]=useReducer(Reducer,defaultState)
 
 const CartProvider = (props) => {
-  const addItemToCartHandler = (item) => {};
-  const removeItemToCartHandler = (item) => {};
+  // cartState:always to be the latest state
+  // dispatchCartAction: Accept args and update state
+  const [cartState, dispatchCartAction] = useReducer(
+    cartReducer,
+    defaultCartState
+  );
+
+  // Two kinds of states, add a new item or add a existed item
+  // For new item, add an item object and a number
+  // For the existed item, need update the number
+  // Be aware, the item is an object:  {name: sushi, amount:5}
+  const addItemToCartHandler = (item) => {
+    // Each reducer should follow a fixed format
+    // Here I use this format: ActionType, Data
+    dispatchCartAction({ type: "ADD", item });
+  };
+
+  const removeItemToCartHandler = (item) => {
+    dispatchCartAction({ type: "REMOVE", item });
+  };
+
+  // this is the data shared with context
   const cartContext = {
-    items: [],
-    totalAmount: 0,
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
     addItem: addItemToCartHandler,
     removeItem: removeItemToCartHandler,
   };
+
   return (
     <CartContext.Provider value={cartContext}>
       {props.children}
